ca6a7b9dcc4a18b149be1571e1154c8509258c3d
Dylan Griffith <dyl.griffith@gmail.com>
2016-09-24 17:06:57 -0700

    Generated project from Spring Initializr with Web

---
+++ src/main/java/net/dylangriffith/SpringboottestingApplication.java
@@ -0,0 +1,12 @@
+package net.dylangriffith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringboottestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringboottestingApplication.class, args);
+	}
+}
